7930aeefc5f8bf831bec69f0063cda305999f375
Vitaly Rybakov <vita250r@yandex.ru>
2025-08-16 17:04:39 +0300

    Инициализация проекта

---
+++ src/main/java/ru/ryb/budget/ExpensesIncomeApplication.java
@@ -0,0 +1,13 @@
+package ru.ryb.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesIncomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesIncomeApplication.class, args);
+	}
+
+}
